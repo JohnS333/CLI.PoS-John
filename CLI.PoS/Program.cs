b1b4561568e685c9a081cb4f3c4c68cb5cb13b8d
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2026-01-30 09:43:15 -0500

    adds menu loops for a user to determine the end of a session. Closes #14

--- CLI.PoS/Program.cs
+++ CLI.PoS/Program.cs
@@ -12,45 +12,53 @@ namespace MyApp
             // list is a copy of of the items in the service, so when we add to it, it adds to the service as well 
             // satic access is good but it can cost a lot of ram if the service is large.
             var list = ItemServiceProxy.Current.Items;
-
-            Console.WriteLine("Choose one of the following:");
-            Console.WriteLine("1. Administrator");
-            Console.WriteLine("2. User");
-
-            var choice = Console.ReadLine();
-            if(int.TryParse(choice, out int choiceInt))
+            var choice = string.Empty;
+            do
             {
-                switch (choiceInt)
+                Console.WriteLine("Choose one of the following:");
+                Console.WriteLine("1. Administrator");
+                Console.WriteLine("2. User");
+                Console.WriteLine("3. Quit");
+
+                choice = Console.ReadLine();
+                if (int.TryParse(choice, out int choiceInt))
                 {
-                    case 1:
-                        Console.WriteLine("Admin Menu");
-                        Console.WriteLine("C. Create New Menu Item");
-                        Console.WriteLine("U. Edit Menu Item");
-
-                        var subChoice = Console.ReadLine();
-                        if (subChoice.Equals("C", StringComparison.InvariantCultureIgnoreCase))
+                    var subChoice = string.Empty;
+                    do
+                    {
+                        switch (choiceInt)
                         {
-                            Console.WriteLine("Name:");
-                            var name = Console.ReadLine();
-                            Console.WriteLine("Description:");
-                            var description = Console.ReadLine();
+                            case 1:
+                                Console.WriteLine("Admin Menu");
+                                Console.WriteLine("C. Create New Menu Item");
+                                Console.WriteLine("U. Edit Menu Item");
+                                Console.WriteLine("Q. Quit");
 
-                            Console.WriteLine("Price:");
-                            var price = Console.ReadLine();
+                                subChoice = Console.ReadLine();
+                                if (subChoice.Equals("C", StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    Console.WriteLine("Name:");
+                                    var name = Console.ReadLine();
+                                    Console.WriteLine("Description:");
+                                    var description = Console.ReadLine();
 
-                            var item = new Item
-                            {
-                                Name = name,
-                                Description = description,
-                                Price = decimal.Parse(price)
-                            };
-                            ItemServiceProxy.Current.AddOrUpdate(item);
+                                    Console.WriteLine("Price:");
+                                    var price = Console.ReadLine();
 
-                            Console.WriteLine(item);
-                        } else if(subChoice.Equals("U", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                            //display the items in their current state
-                            list.ForEach(Console.WriteLine);
+                                    var item = new Item
+                                    {
+                                        Name = name,
+                                        Description = description,
+                                        Price = decimal.Parse(price)
+                                    };
+                                    ItemServiceProxy.Current.AddOrUpdate(item);
+
+                                    Console.WriteLine(item);
+                                }
+                                else if (subChoice.Equals("U", StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    //display the items in their current state
+                                    list.ForEach(Console.WriteLine);
 
                             //let a user choose which one to update
                             //if Console.ReadLine() is null, we will default to 0
@@ -60,40 +68,43 @@ namespace MyApp
                             var itemToEdit = list.FirstOrDefault(i => i.Id == editChoice);
                             //"Default" for itemToEdit is null, because item is a reference type 
 
-                            if (itemToEdit != null)
-                            {
-                                //make the update
-                                Console.WriteLine("New Name:");
-                                var newName = Console.ReadLine();
-                                if (!string.IsNullOrEmpty(newName))
-                                {
-                                    itemToEdit.Name = newName;
-                                }
-                                Console.WriteLine("New Price:");
-                                var newPrice = Console.ReadLine();
-                                if (!string.IsNullOrEmpty(newPrice))
-                                {
-                                    itemToEdit.Price = decimal.Parse(newPrice);
-                                }
+                                    if (itemToEdit != null)
+                                    {
+                                        //make the update
+                                        Console.WriteLine("New Name:");
+                                        var newName = Console.ReadLine();
+                                        if (!string.IsNullOrEmpty(newName))
+                                        {
+                                            itemToEdit.Name = newName;
+                                        }
+                                        Console.WriteLine("New Price:");
+                                        var newPrice = Console.ReadLine();
+                                        if (!string.IsNullOrEmpty(newPrice))
+                                        {
+                                            itemToEdit.Price = decimal.Parse(newPrice);
+                                        }
 
 
-                                ItemServiceProxy.Current.AddOrUpdate(itemToEdit);
-                            }
+                                        ItemServiceProxy.Current.AddOrUpdate(itemToEdit);
+                                    }
+                                }
+                                break;
+                            case 2:
+                                Console.WriteLine("User Menu");
+                                break;
+                            case 3:
+                                break;
+                            default:
+                                Console.WriteLine("ERROR: Unknown User Type");
+                                break;
                         }
-                        break;
-                    case 2:
-                        Console.WriteLine("User Menu");
-                        break;
-                    default:
-                        Console.WriteLine("ERROR: Unknown User Type");
-                        break;
+                    } while (!subChoice.Equals("Q", StringComparison.InvariantCultureIgnoreCase)
+                            && choiceInt != 3
+                        );
                 }
 
-               
-            }
 
-
-            
+            } while (!choice.Equals("3", StringComparison.OrdinalIgnoreCase));
         }
     
         static void PrintStudentMenu()
