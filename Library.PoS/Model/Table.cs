aed3cefaaa6d623451ac92e2da4ccdac962a4388
crmillsfsu <97139671+crmillsfsu@users.noreply.github.com>
2026-03-04 09:48:09 -0500

    Finished table add and delete. Closes #36

--- Library.PoS/Model/Table.cs
+++ Library.PoS/Model/Table.cs
@@ -25,6 +25,7 @@ namespace Library.PoS.Model
         public TableState Status { get; set;  }
         public List<DateTime> StatusChanges {  get; set; }
         public bool? IsReserved { get; set;  }
+        public int Id { get; set; }
     }
 
     public enum TableState
