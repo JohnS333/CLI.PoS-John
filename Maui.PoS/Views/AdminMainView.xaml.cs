69428331d1102166f0f60e630bc968c7cb5109de
John Simson <johnsimson101@outlook.com>
2026-04-04 15:16:53 -0400

    notes

--- Maui.PoS/Views/AdminMainView.xaml.cs
+++ Maui.PoS/Views/AdminMainView.xaml.cs
@@ -16,6 +16,7 @@ public partial class AdminMainView : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = this;
+		// " BindingContext=this" means this view itself is its own binding context. So we can put properties on this class that store the data we want to display. This violates MVVM, but its fine for now.
 	}
 
     private void GoBackClicked(object sender, EventArgs e)
