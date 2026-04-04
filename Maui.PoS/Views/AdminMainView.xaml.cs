e370f6215b475ee8044c218df24c39ae85e940e5
John Simson <johnsimson101@outlook.com>
2026-04-04 15:02:03 -0400

    john comments feb 9 p1

--- Maui.PoS/Views/AdminMainView.xaml.cs
+++ Maui.PoS/Views/AdminMainView.xaml.cs
@@ -10,5 +10,6 @@ public partial class AdminMainView : ContentPage
     private void GoBackClicked(object sender, EventArgs e)
     {
 		Shell.Current.GoToAsync("//MainPage");
+        // the "//" is used to navigate to the root of the namespace visual tree.
     }
 }
