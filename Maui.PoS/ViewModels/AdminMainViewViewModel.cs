ea63f4674d546d4966009344f4969c76fe7f38f2
John Simson <johnsimson101@outlook.com>
2026-04-17 16:36:13 -0400

    john feb 13 notes

--- Maui.PoS/ViewModels/AdminMainViewViewModel.cs
+++ Maui.PoS/ViewModels/AdminMainViewViewModel.cs
@@ -11,6 +11,9 @@ namespace Maui.PoS.ViewModels
 {
     public class AdminMainViewViewModel: INotifyPropertyChanged
     {
+        // InotifyPropertyChanged is an interface. Having it on the right of this semicolon means this class implements the interface.
+        // When a class implements an interface, it agrees to a contract. The interface dictates a specific set of members that MUST exist
+        // INotifyPropertyChanged interface requires any class that implements it to contain an event named PropertyChanged
         public ObservableCollection<Item> Items
         {
             get
@@ -20,8 +23,16 @@ namespace Maui.PoS.ViewModels
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
+            // this event comes from the INotifyPropertyChanged interface,
+            // "PropertyChangedEventHandler" is a built in delegate type in .NET. it comes from the System.ComponentModel namespace
+            // a delegate in C# is a type that defines a specifci method signature.
+            //    In this case, PropertyChangedEventHandler dictates the shape of any method that wants to subscribe to (or "listen" to) the PropertyChanged event.
+            //Specifically, it requires that the handling method must take two parameters:
+            //1.	object? sender: The object that triggered the event (in this case, your ViewModel).
+            //2.	PropertyChangedEventArgs e: An object containing the event data (specifically, the name of the property that changed).
+            // "Any method that wants to be attached to this event MUST accept two inputs (parameters): one object (the sender), and one PropertyChangedEventArgs (the event data)."
 
-        public void Refresh()
+    public void Refresh()
         {
             NotifyPropertyChanged("Items");
         }
