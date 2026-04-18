00174db8f43529ce4ff8e4be7e7e0da5742fd315
John Simson <johnsimson101@outlook.com>
2026-04-18 16:22:18 -0400

    john feb 16 notes

--- Maui.PoS/Views/ItemDetailView.xaml.cs
+++ Maui.PoS/Views/ItemDetailView.xaml.cs
@@ -23,6 +23,8 @@ public partial class ItemDetailView : ContentPage
 
     private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
     {
+        // if you re instate the binding context in the constructor, MAui will not update it because its trying to save resources.
+        // Its smarter to update the binding context in the navigated to event.
         BindingContext = new Item();
     }
 }
